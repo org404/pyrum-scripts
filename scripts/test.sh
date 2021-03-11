215453184d43234981fb6d648682989682787819
kittyandrew <kitty@maudrew.dev>
2021-03-12 01:21:22 +0200

    preparation for raw requests

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,6 +7,8 @@ export TID="16Uiu2HAm2zw4bWPboDzFZqwKmsXoVbYozWWkxU5onqxaxGTjsxXJ"
 # export P2P_PUBLIC_IP=$(curl -s4 ifconfig.io)
 # export TADDRESS="/ip4/0.0.0.0/tcp/9001/p2p/16Uiu2HAmL6ZyE81ABAX9qLcgt1LHSunD5633b1oaQKNpkd1nPmEu"
 # export TID="16Uiu2HAmL6ZyE81ABAX9qLcgt1LHSunD5633b1oaQKNpkd1nPmEu"
-echo -e "$P2P_PUBLIC_IP - $TADDRESS - $TID"
+
+export FORK_DIGEST="TODO:SET_FORK_DIGEST"
+echo -e "$P2P_PUBLIC_IP - $TADDRESS - $TID - $FORK_DIGEST"
 # ./rumor file test.rumor
 ./rumor shell --level debug
