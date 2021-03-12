06b4d152a748943b9f6ba0df85b5dec806407e8d
kittyandrew <kitty@maudrew.dev>
2021-03-12 23:56:31 +0200

    manual fix for fork digest

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,13 @@ export TID="16Uiu2HAm2zw4bWPboDzFZqwKmsXoVbYozWWkxU5onqxaxGTjsxXJ"
 # export TADDRESS="/ip4/0.0.0.0/tcp/9001/p2p/16Uiu2HAmL6ZyE81ABAX9qLcgt1LHSunD5633b1oaQKNpkd1nPmEu"
 # export TID="16Uiu2HAmL6ZyE81ABAX9qLcgt1LHSunD5633b1oaQKNpkd1nPmEu"
 
-export FORK_DIGEST="TODO:SET_FORK_DIGEST"
-echo -e "$P2P_PUBLIC_IP - $TADDRESS - $TID - $FORK_DIGEST"
+#
+# To set this I've used modified version of synced node
+# and just encoded and printed correct fork digest.
+#
+export FORK_DIGEST="0x3b088795"
+export FORK_DIGEST_NO_PREFIX=${FORK_DIGEST#"0x"}
+# General info
+echo -e "$P2P_PUBLIC_IP - $TADDRESS - $TID - $FORK_DIGEST - $FORK_DIGEST_NO_PREFIX"
 # ./rumor file test.rumor
 ./rumor shell --level debug
